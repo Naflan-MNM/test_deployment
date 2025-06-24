602baf43842d2a5d307840afc8265d4df8a8ae25
Naflan-MNM <naflanm084@gmail.com>
2025-06-24 19:42:27 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,html}", // Adjust as per your project structure
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
