0f4db9df5e663c1daf39ce723376d6587dbc129d
Naflan-MNM <naflanm084@gmail.com>
2025-07-25 21:34:46 +0530

    enhance animations

--- src/App.jsx
+++ src/App.jsx
@@ -97,10 +97,10 @@ function App() {
       </main>
 
       {/* Background Elements */}
-      {/* <div className="fixed inset-0 pointer-events-none">
+      <div className="fixed inset-0 pointer-events-none">
         <div className="absolute top-1/4 left-1/4 w-96 h-96 bg-blue-500 rounded-full mix-blend-multiply filter blur-xl opacity-5 animate-pulse"></div>
         <div className="absolute bottom-1/4 right-1/4 w-96 h-96 bg-[rgba(26,173,156,0.7)] rounded-full mix-blend-multiply filter blur-xl opacity-10 animate-pulse animation-delay-2000"></div>
-      </div> */}
+      </div>
     </div>
   );
 }
